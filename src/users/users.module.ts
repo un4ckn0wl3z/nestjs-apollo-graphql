4fa52be0dcab2078185c78f657b8e6a0dae6d34a
un4kn0wl3z <admin@unknownclub.net>
2023-02-08 10:35:32 +0700

    fix: set PhotosApiService as provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PhotosApiService } from "./photo-api.service";
 import { UsersResolver } from "./users.resolver";
 import { UsersService } from "./users.service";
 
 @Module({
     imports: [
     ],
-    providers: [UsersResolver, UsersService]
+    providers: [UsersResolver, UsersService, PhotosApiService]
 })
 export class UsersModule {}
